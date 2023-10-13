b298dcb038db0ffab2abe09c9be8d40c7b2e0d0a
Kid0601 <love19970602@gmail.com>
2023-10-13 18:05:06 +0800

    優惠券後端

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ import usersRouter from './routes/users.js'
 import googleLoginRouter from './routes/google-login.js'
 import lineLoginRouter from './routes/line-login.js'
 import facebookLoginRouter from './routes/facebook-login.js'
+import couponRouter from './routes/coupon.js'
 
 import favoriteRouter from './routes/favorite.js'
 
@@ -93,6 +94,7 @@ app.use('/api/google-login', googleLoginRouter)
 app.use('/api/line-login', lineLoginRouter)
 app.use('/api/facebook-login', facebookLoginRouter)
 app.use('/api/favorite', favoriteRouter)
+app.use('/api/coupon', couponRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/coupon.js
@@ -0,0 +1,128 @@
+import express from 'express'
+const router = express.Router()
+import { getUserById } from '../models/users.js'
+import {
+  getCoupon,
+  getCouponWithQS,
+  getCouponById,
+  countWithQS,
+  updateUserCoupon,
+} from '../models/coupon.js'
+import authenticate from '../middlewares/jwt.js'
+import { executeQuery } from '../models/base.js'
+import pool from '../config/db.js'
+
+// 讀取該會員優惠券
+router.get('/', authenticate, async (req, res) => {
+  console.log('coupon_test_authenticate')
+  updateUserCoupon(req.user.id)
+  // const sql = `SELECT user_coupon.coupon_id, coupon.*
+  // FROM user_coupon
+  // JOIN coupon ON user_coupon.coupon_id = coupon.id
+  // WHERE user_coupon.user_id = ${req.user.id} AND coupon.is_valid = 1 AND coupon.start_date > NOW()`
+  const sql = `SELECT user_coupon.coupon_id, coupon.*
+  FROM user_coupon
+  JOIN coupon ON user_coupon.coupon_id = coupon.id
+  WHERE user_coupon.user_id = ${req.user.id} AND coupon.is_valid = 1 AND user_coupon.status = 1 `
+  const { rows } = await executeQuery(sql)
+  const coupon = rows.map((v) => ({
+    couponId: v.id,
+    coupon_code: v.coupon_code,
+    coupon_name: v.coupon_name,
+    description: v.description,
+    threshold: v.threshold,
+    discount_percent: v.discount_percent,
+    discount_value: v.discount_value,
+    end_date: v.end_date,
+  }))
+  // return res.json({ message: 'authorized', rows, user })
+  return res.json({ message: 'authorized', coupon })
+})
+
+// 讀取已過期
+router.get('/couponExpired', authenticate, async (req, res) => {
+  console.log('coupon_Expired_authenticate')
+  const sql = `SELECT user_coupon.coupon_id, coupon.*
+  FROM user_coupon
+  JOIN coupon ON user_coupon.coupon_id = coupon.id
+  WHERE user_coupon.user_id = ${req.user.id} AND coupon.is_valid = 1 AND user_coupon.status = 0`
+  // const user = req.user.vip
+  console.log(sql)
+  const { rows } = await executeQuery(sql)
+  const couponExpired = rows.map((v) => ({
+    couponId: v.id,
+    coupon_code: v.coupon_code,
+    coupon_name: v.coupon_name,
+    description: v.description,
+    threshold: v.threshold,
+    discount_percent: v.discount_percent,
+    discount_value: v.discount_value,
+    end_date: v.end_date,
+  }))
+  return res.json({ message: 'authorized', couponExpired })
+})
+
+// 讀取已使用
+router.get('/couponUsed', authenticate, async (req, res) => {
+  console.log('coupon_Used_authenticate')
+  const sql = `SELECT user_coupon.coupon_id, coupon.*
+  FROM user_coupon
+  JOIN coupon ON user_coupon.coupon_id = coupon.id
+  WHERE user_coupon.user_id = ${req.user.id} AND coupon.is_valid = 1 AND user_coupon.status = 2`
+  const { rows } = await executeQuery(sql)
+  const couponUsed = rows.map((v) => ({
+    couponId: v.id,
+    coupon_code: v.coupon_code,
+    coupon_name: v.coupon_name,
+    description: v.description,
+    threshold: v.threshold,
+    discount_percent: v.discount_percent,
+    discount_value: v.discount_value,
+    end_date: v.end_date,
+  }))
+  return res.json({ message: 'authorized', couponUsed })
+})
+
+// 新增優惠碼
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  // 從要求的req.body獲取couponCode
+  const { couponCode, userId } = req.body
+  if (couponCode == '') {
+    return res.json({ message: 'fail', code: '401' })
+  }
+  // 先查詢資料庫是否有同couponCode的資料
+  const findCouponId = `SELECT id FROM coupon WHERE coupon_code = '${couponCode}'`
+  const [result, fields] = await pool.execute(findCouponId)
+  if (!result.length) {
+    return res.json({ message: 'fail', code: '400' })
+  }
+
+  // 查詢該優惠碼是否過期
+  const findIsExpiredCoupon = `SELECT end_date FROM coupon WHERE coupon_code = '${couponCode}'`
+  const [result2, fields2] = await pool.execute(findIsExpiredCoupon)
+  console.log(result2[0])
+  console.log(new Date().toISOString().split('T')[0])
+  console.log(result2[0].end_date < new Date().toISOString().split('T')[0])
+  if (result2[0].end_date < new Date().toISOString().split('T')[0]) {
+    return res.json({ message: 'fail', code: '403' })
+  }
+
+  // 查詢該優惠碼是否已經輸入過
+  const findDuplicateCoupon = `SELECT id FROM user_coupon WHERE coupon_id = ${result[0].id}`
+  const [result1, fields1] = await pool.execute(findDuplicateCoupon)
+  console.log(result1.length)
+  if (result1.length) {
+    return res.json({ message: 'fail', code: '402' })
+  }
+
+  // 新增該優惠碼的id到user_coupon
+  const updateUserCoupon = `INSERT INTO user_coupon (user_id, coupon_id, status)
+  SELECT u.id,${result[0].id} , 1
+  FROM users u
+  WHERE u.id = ${userId}`
+  await pool.execute(updateUserCoupon)
+  return res.json({ message: 'success', code: '200' })
+})
+
+export default router
