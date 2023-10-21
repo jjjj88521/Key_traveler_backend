e0ddff22e20d4036aa47121729324031d696a4fa
jjjj88521 <jjjj88521@gmail.com>
2023-10-22 05:43:30 +0800

    修改信件樣式

--- routes/email-user.js
+++ routes/email-user.js
@@ -4,16 +4,130 @@ import 'dotenv/config.js'
 
 const router = express.Router()
 
+const mailHtml = (content) => {
+  content = content.replace(/\r\n/g, '<br>')
+  return `
+  <html>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;600;700;800;900&family=Poppins&display=swap"
+        rel="stylesheet"
+      />
+      <style>
+        body {
+          font-family: 'poppins', 'Noto Sans', 'Noto Sans TC Regular', Helvetica,
+            Arial, 'PingFang TC', '苹方-繁', 'Heiti TC', '黑體-繁',
+            'Microsoft JhengHei', '微軟正黑體', system-ui, -apple-system,
+            'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans',
+            'Liberation Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+            'Segoe UI Symbol', 'Noto Color Emoji';
+          font-size: 16px;
+          line-height: 1.5;
+          box-sizing: border-box;
+        }
+  
+        .mail-wrapper {
+          background-color: rgb(240, 240, 240);
+          padding: 30px 0;
+          width: 100%;
+          color: #171717;
+        }
+  
+        .mail-card {
+          margin: auto;
+          padding: 20px;
+          background-color: white;
+          border: 20px solid #dc942975;
+          width: 70%;
+        }
+  
+        .title {
+          font-weight: 900;
+          font-size: 28px;
+        }
+  
+        .content {
+          padding: 5px;
+          margin-block: 10px;
+          border: 1px solid gray;
+        }
+  
+        .token-content {
+          background-color: #dc9429;
+          color: white;
+          padding-block: 10px;
+        }
+  
+        .tip {
+          text-align: center;
+          font-size: 20px;
+          font-weight: 900;
+        }
+  
+        .token {
+          text-align: center;
+          font-size: 30px;
+          font-weight: 900;
+        }
+  
+        .column {
+          width: 100%;
+        }
+  
+        .img-wrapper img {
+          display: block;
+          margin: auto;
+          width: 50%;
+          max-width: 300px;
+        }
+      </style>
+    </head>
+  
+    <body>
+      <div class="mail-wrapper">
+        <div class="mail-card">
+          <h2 class="title">親愛的 鍵之旅人 會員您好：</h2>
+  
+          <p class="content">
+            ${content}
+            <br/>
+            如有任何問題請洽 鍵之旅人 客服人員
+            <br />
+            <br />
+            <a href="mailto:ispankeytraveler@gmail.com"
+              >聯絡客服人員 ispankeytraveler@gmail.com</a
+            >
+          </p>
+  
+          <h2>敬上</h2>
+          <h2>Key Traveler 鍵之旅人</h2>
+          <div class="img-wrapper">
+            <img
+              src="https://i.imgur.com/GvIWCUj.png"
+              alt="Key Traveler Logo"
+            />
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+  `
+}
+
 /* 寄送email的路由 */
 router.post('/send', function (req, res, next) {
   const email = req.body.email
   console.log(email)
+  const mailContent =
+    '歡迎加入鍵之旅人的行列。我們非常高興您選擇了我們的服務。\r\n感謝您的支持!\r\n祝您有個愉快的指尖旅程!\r\n'
   // email內容
   const mailOptions = {
-    from: `"key-traveler"<${process.env.SMTP_TO_EMAIL}>`,
+    from: `"鍵之旅人"<${process.env.SMTP_TO_EMAIL}>`,
     to: `${email}`,
     subject: '歡迎註冊 鍵之旅人',
-    text: `親愛的用戶，\r\n\r\n歡迎加入鍵之旅人的行列。我們非常高興您選擇了我們的服務。\r\n感謝您的支持，如果您有任何問題或需要幫助，請隨時聯絡我們。\r\n\r\n敬上\r\n鍵之旅人`,
+    html: mailHtml(mailContent),
   }
 
   // 寄送
--- routes/email.js
+++ routes/email.js
@@ -4,14 +4,132 @@ import 'dotenv/config.js'
 
 const router = express.Router()
 
+const mailTest = (otpToken) => `
+<html>
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;600;700;800;900&family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <style>
+      body {
+        font-family: 'poppins', 'Noto Sans', 'Noto Sans TC Regular', Helvetica,
+          Arial, 'PingFang TC', '苹方-繁', 'Heiti TC', '黑體-繁',
+          'Microsoft JhengHei', '微軟正黑體', system-ui, -apple-system,
+          'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans',
+          'Liberation Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+          'Segoe UI Symbol', 'Noto Color Emoji';
+        font-size: 16px;
+        line-height: 1.5;
+        box-sizing: border-box;
+      }
+
+      .mail-wrapper {
+        background-color: rgb(240, 240, 240);
+        padding: 60px 0;
+        width: 100%;
+        color: #171717;
+      }
+
+      .mail-card {
+        margin: auto;
+        padding: 20px;
+        background-color: white;
+        border: 20px solid #dc942975;
+        width: 50%;
+      }
+
+      .title {
+        font-weight: 900;
+        font-size: 28px;
+      }
+
+      .content {
+        padding: 5px;
+        margin-block: 10px;
+        border: 1px solid gray;
+      }
+
+      .token-content {
+        background-color: #dc9429;
+        color: white;
+        padding-block: 10px;
+      }
+
+      .tip {
+        text-align: center;
+        font-size: 20px;
+        font-weight: 900;
+      }
+
+      .token {
+        text-align: center;
+        font-size: 30px;
+        font-weight: 900;
+      }
+
+      .column {
+        width: 100%;
+      }
+
+      .img-wrapper img {
+        display: block;
+        margin: auto;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="mail-wrapper">
+      <div class="mail-card">
+        <h2 class="title">親愛的 鍵之旅人 會員您好：</h2>
+
+        <p class="content">
+          通知重設密碼所需要的驗證碼，<br />
+          將以下6位驗證碼輸入於：<br />
+          重設密碼頁面的《電子郵件驗證碼》欄位中。<br />
+          <br />
+          請注意驗證碼將於寄送後 30 分鐘後過期，<br />
+          如有任何問題請洽 鍵之旅人 客服人員
+          <br />
+          <br />
+          <a href="mailto:ispankeytraveler@gmail.com"
+            >聯絡客服人員 ispankeytraveler@gmail.com</a
+          >
+        </p>
+
+        <div class="token-content">
+          <div class="column">
+            <div class="tip">請輸入以下的6位數字：</div>
+            <div class="token">${otpToken}</div>
+          </div>
+        </div>
+
+        <h2>敬上</h2>
+        <h2>Key Traveler 鍵之旅人</h2>
+        <div class="img-wrapper">
+          <img
+            src="https://i.imgur.com/GvIWCUj.png"
+            alt="Key Traveler Logo"
+            width="300"
+          />
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+`
+
 /* 寄送email的路由 */
 router.get('/send', function (req, res, next) {
   // email內容
   const mailOptions = {
-    from: `"key-traveler"<${process.env.SMTP_TO_EMAIL}>`,
-    to: `f2erobinchen@gmail.com`,
+    from: `"鍵之旅人"<${process.env.SMTP_TO_EMAIL}>`,
+    to: `jjjj88521@gmail.com`,
     subject: '這是一封測試電子郵件',
-    text: `你好， \r\n通知你有關第一封郵件的事。\r\n\r\n敬上\r\n鍵之旅人`,
+    html: mailTest(666666),
   }
 
   // 寄送
